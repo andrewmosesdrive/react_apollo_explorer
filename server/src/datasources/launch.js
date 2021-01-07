636f90a81014f5d07b9673f9cc6f4d8e84ad52c9
Andrew Moses <andrewmosesdrive@gmail.com>
2021-01-06 23:20:01 -0700

    launch api methods created

---
+++ server/src/datasources/launch.js
@@ -0,0 +1,50 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+class LaunchAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.spacexdata.com/v2/";
+  }
+
+  // get method; if launches map out and use launchReducer, else return empty array
+  async getAllLaunhes() {
+    const response = await this.get("launches");
+    return Array.isArray(response)
+      ? response.map((launch) => this.launchReducer(launch))
+      : [];
+  }
+
+  // create method to transform data from get into schema shape
+  launchReducer(launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_data_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
+
+  // take flight number and return data for associated launch (single)
+  async getLaunchById({ launchId }) {
+    const response = await this.get("launches", { flight_number: launchId });
+    return this.launchReducer(response[0]);
+  }
+
+  // multiple calls
+  getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map((launchId) => this.getLaunchById({ launchId }))
+    );
+  }
+}
+
+module.exports = LaunchAPI;
