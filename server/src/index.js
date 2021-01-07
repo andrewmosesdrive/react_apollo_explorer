4a0f1bb9c916905cc4cbd2ef334743e3f15a4fa9
Andrew Moses <andrewmosesdrive@gmail.com>
2021-01-06 22:00:41 -0700

    updated index.js for apollo-server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,5 @@
-require('dotenv').config();
+require("dotenv").config();
+// import ApolloServer class and schema; create new instance of apolloserver and pass schema
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const server = new ApolloServer({ typeDefs });
