7e598ca2282e6e6ae6533a6eba2bf84b7f070eab
Andrew Moses <andrewmosesdrive@gmail.com>
2021-02-17 00:35:33 -0700

    corrected server error; API responding properly

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 // import ApolloServer class and schema; create new instance of apolloserver and pass schema
-const { ApolloServer } = require("apollo-server");
-const typeDefs = require("./schema");
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
 const { createStore } = require('./utils');
 const resolvers = require('./resolvers');
 
@@ -11,15 +11,18 @@ const UserAPI = require('./datasources/user');
 const store = createStore();
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-        LaunchAPI: new LaunchAPI(),
-        UserAPI: new UserAPI({ store })
-    })
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
 });
 
 server.listen().then(() => {
-    console.log(`
-    BIG YEET! Listening on http://localhost:4000; graph at https://studio.apollographql.com/dev`)
-})
+  console.log(`
+    BIG YEET! Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/dev
+  `);
+});
